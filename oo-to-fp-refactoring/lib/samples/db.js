18b3c9c9ff4a313f2c561521caa7ee65c8435d0e
Shawn Button <shawn.button@bell.ca>
2019-11-01 09:59:28 -0400

    added more examples

--- oo-to-fp-refactoring/lib/samples/db.js
+++ oo-to-fp-refactoring/lib/samples/db.js
@@ -1,6 +1,14 @@
 'use strict'
 
+const fs = require('fs')
+
 const find = id => ({ active: true })
 const save = record => ({ })
 
+const writeToFile = data => fs.writeFile('file.txt', data)
+
 module.exports = { find, save }
+
+const getRandom = () => Math.random()
+
+console.log(getRandom())
